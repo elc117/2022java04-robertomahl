8a1bad6883b2985a443625ddec27091f8cdbed5c
robertomahl <75228333+robertomahl@users.noreply.github.com>
2022-06-27 15:59:18 -0300

    Add Vaca

---
+++ VacaMain.java
@@ -0,0 +1,12 @@
+package pratica;
+
+public class VacaMain {
+	
+	public static void main(String[] args) {
+		Vaca mimosa = new Vaca("grama", "Mimosa");
+		mimosa.Alimentar("tomate");
+		mimosa.Alimentar("grama");
+		mimosa.mujir();
+	}
+	
+}
